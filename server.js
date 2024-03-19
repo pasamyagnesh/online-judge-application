46281b39484b8d71daccb43becbc38f5573b53e4
yagnesh2004 <93670699+yagnesh2004@users.noreply.github.com>
2024-03-19 17:53:37 +0530

    Installed

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Yagnesh ");
+const express = require('express');
+
+const app = express();
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+
+app.get('/result', (req, res) => {
+  res.render("result");
+});
+app.get('/', (req, res) => {
+    res.render("input");
+  });
+
+  app.get('/solve', (req, res) => {
+    res.render("input");
+  });
+  
+  
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
